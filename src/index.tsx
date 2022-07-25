90ff0ba40dbe0dcc9aa5a3b6b86e8a2705aba02d
HiepLe1999 <107990782+HiepLe1999@users.noreply.github.com>
2022-07-25 17:21:28 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.min.css";
+import { ClassStudent } from "./components/Student/ClassStudent";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ListStudent } from "./components/Student/ListStudent";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />} >
+    <Route path="/liststudent" element={<ListStudent />} />
+    <Route path="/classstudent" element={<ClassStudent />} />
+    </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
